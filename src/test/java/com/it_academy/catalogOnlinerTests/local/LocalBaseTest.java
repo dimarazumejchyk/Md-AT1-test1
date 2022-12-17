4a2f44789b7b5e472251dfc49a6e03e547266498
DzmitryRazumejchyk <108740076+dimarazumejchyk@users.noreply.github.com>
2022-12-17 14:17:10 +0300

    Commit11 allure to local suite 1

--- src/test/java/com/it_academy/catalogOnlinerTests/local/LocalBaseTest.java
+++ src/test/java/com/it_academy/catalogOnlinerTests/local/LocalBaseTest.java
@@ -1,14 +1,19 @@
 package com.it_academy.catalogOnlinerTests.local;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.it_academy.catalogOnlinerTests.pageobject.HomePage;
+import com.it_academy.listeners.AllureListener;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 
 import static com.it_academy.catalogOnlinerTests.driverManager.WebDriverLocal.setDriver;
 
+@Listeners(AllureListener.class)
 
 public class LocalBaseTest {
     private final static String ONLINER_URL = "https://www.onliner.by/";
@@ -19,6 +24,7 @@ public class LocalBaseTest {
     @Parameters(value = {"browser"})
     public void webDriverInit(String browser) {
         LOG.info("BeforeClass LocalBaseTest");
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
         setDriver(browser);
         HomePage homePage = new HomePage();
         homePage.openOnliner(ONLINER_URL);
